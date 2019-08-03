7b2d46ff31af142a584332375858e12de2062ec9
James Davey <james.t.davey@gmail.com>
2019-08-03 09:55:18 -0700

    complete nwc loop

--- Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-  int max = 1000;
-  int min = 1;
-  int guess = 500;
-  void createNewGuess()
-  {
-    guess = (max + min) / 2;
-    Debug.Log("Okay. Is it higher or lower than... " + guess + "?");
-  }
+  int max;
+  int min;
+  int guess;
   // Start is called before the first frame update
   void Start()
   {
@@ -20,6 +15,9 @@ public class NumberWizard : MonoBehaviour
 
   void StartGame()
   {
+    max = 1000;
+    min = 1;
+    guess = 500;
     Debug.Log("Hey. Check out the this number wizard.");
     Debug.Log("Pick a number. Don't tell me what it is.");
     Debug.Log("The highest number you can pick is: " + max + ".");
@@ -36,17 +34,23 @@ public class NumberWizard : MonoBehaviour
     {
       // Debug.Log("Up Arrow key was pressed.");
       min = guess;
-      createNewGuess();
+      NextGuess();
     }
     else if (Input.GetKeyDown(KeyCode.DownArrow))
     {
       // Debug.Log("Down Arrow key was pressed.");
       max = guess;
-      createNewGuess();
+      NextGuess();
     }
     else if (Input.GetKeyDown(KeyCode.Return))
     {
       Debug.Log("Haha I fucking knew it.");
+      StartGame();
     }
+  }
+  void NextGuess()
+  {
+    guess = (max + min) / 2;
+    Debug.Log("Okay. Is it higher or lower than... " + guess + "?");
   }
 }
